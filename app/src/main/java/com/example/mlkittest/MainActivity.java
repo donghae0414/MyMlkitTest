0e468ff54bdf256b54909840c8405bf955ed35d1
Junyoung Park <carpe0308@naver.com>
2019-10-14 03:42:33 +0900

    여러 사진 받았을 때 처리할 수 있도록 수정

--- app/src/main/java/com/example/mlkittest/MainActivity.java
+++ app/src/main/java/com/example/mlkittest/MainActivity.java
@@ -1,137 +1,138 @@
 package com.example.mlkittest;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.ml.vision.FirebaseVision;
-import com.google.firebase.ml.vision.common.FirebaseVisionImage;
-import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
-import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
-
-import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static int PICK_IMAGE_MULTIPLE = 1;
+    List<Uri> imageUris;
+
     ImageView imageView;
     Button button;
     TextView textView;
 
     Bitmap bitmap;
-    FirebaseVisionImage firebaseImage;
-    FirebaseVisionImageLabeler labeler;
-    long start;
-    long end;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = (ImageView) findViewById(R.id.imageView);
-        button = (Button) findViewById(R.id.button);
-        textView = (TextView) findViewById(R.id.textView);
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cat_dog);
-
-        labeler = FirebaseVision.getInstance().getOnDeviceImageLabeler();
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //objectDetection();
-//                //weird album
-//                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-//                intent.setType("image/*");
-//                if (intent.resolveActivity(getPackageManager()) != null) {
-//                    startActivityForResult(intent, 101);
-//                }
-                //we can select gallery app
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
-                startActivityForResult(intent, 101);
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Picture"), PICK_IMAGE_MULTIPLE);
             }
         });
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 101) {
-            if (resultCode == RESULT_OK) {
-                try {
-                    start = System.currentTimeMillis();
-                    InputStream in = getContentResolver().openInputStream(data.getData());
-                    Bitmap bitmap = BitmapFactory.decodeStream(in);
-                    in.close();
-                    imageView.setImageBitmap(bitmap);
-                    firebaseImage = FirebaseVisionImage.fromBitmap(bitmap);
-//                    Uri uri = data.getData();
-//                    firebaseImage = FirebaseVisionImage.fromFilePath(getApplicationContext(), uri);
-//                    imageView.setImageURI(uri);
-                    textView.setText("객체들\n");
-                    labeler.processImage(firebaseImage)
-                            .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
-                                @Override
-                                public void onSuccess(List<FirebaseVisionImageLabel> labels) {
-                                    for (FirebaseVisionImageLabel label : labels) {
-                                        String text = label.getText();
-                                        String entityId = label.getEntityId();
-                                        String confidence = Float.toString(label.getConfidence());
-                                        textView.append("text : " + text + ", entityID : " + entityId + ", confidence : " + confidence + "\n");
-                                    }
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Toast.makeText(getApplicationContext(), "Fail", Toast.LENGTH_LONG).show();
-                                    textView.append("Fail");
-                                }
-                            });
-                    end = System.currentTimeMillis();
-                    textView.append("소요시간 : " + (end - start) + "\n");
-                } catch (Exception e) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        try {
+            // When an Image is picked
+            if (requestCode == PICK_IMAGE_MULTIPLE && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
+
+                imageUris = new ArrayList<Uri>();
+                if(data.getData() != null){
+                    Uri mImageUri = data.getData();
+                    imageUris.add(mImageUri);
+                } else {
+                    if (data.getClipData() != null) {
+                        ClipData mClipData = data.getClipData();
+                        for (int i = 0; i < mClipData.getItemCount(); i++) {
+                            ClipData.Item item = mClipData.getItemAt(i);
+                            Uri uri = item.getUri();
+                            imageUris.add(uri);
+                        }
+                        Log.v("LOG_TAG", "Selected Images" + imageUris.size());
+                    }
                 }
+            } else {
+                Toast.makeText(this, "You haven't picked Image",
+                        Toast.LENGTH_LONG).show();
             }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+                    .show();
         }
+
+        processImagesWithMlkit();
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
-//    public void objectDetection(){ // objectDetection photo in drawable folder
-//        imageView.setImageBitmap(bitmap);
-//        firebaseImage = FirebaseVisionImage.fromBitmap(bitmap);
-//
-//        labeler.processImage(firebaseImage)
-//                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
-//                    @Override
-//                    public void onSuccess(List<FirebaseVisionImageLabel> labels) {
-//                        for(FirebaseVisionImageLabel label : labels){
-//                            String text = label.getText();
-//                            String entityId = label.getEntityId();
-//                            String confidence = Float.toString(label.getConfidence());
-//                            textView.append("text : " + text + ", entityID : " + entityId + ", confidence : " + confidence + "\n");
-//                        }
-//                    }
-//                })
-//                .addOnFailureListener(new OnFailureListener() {
-//                    @Override
-//                    public void onFailure(@NonNull Exception e) {
-//                        Toast.makeText(getApplicationContext(), "Fail", Toast.LENGTH_LONG).show();
-//                        textView.append("Fail");
-//                    }
-//                });
-//    }
+    protected void processImagesWithMlkit() {
+        textView.clearComposingText();
+        for (Uri imageUri: imageUris) {
+            UriLabeler uriLabeler = new UriLabeler(imageUri, new UriLabeler.UriLabelerListener() {
+                @Override
+                public void onSuccess(Uri uri, List<UriLabeler.Label> labels) {
+                    String filename = null;
+                    if (uri.getScheme().equals("content")) {
+                        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+                        try {
+                            if (cursor != null && cursor.moveToFirst()) {
+                                filename = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                            }
+                        } finally {
+                            cursor.close();
+                        }
+                    }
+
+                    if (filename == null) {
+                        filename = uri.getPath();
+                        int cut = filename.lastIndexOf('/');
+                        if (cut != -1) {
+                            filename = filename.substring(cut + 1);
+                        }
+                    }
+
+                    textView.append("{\n" +
+                            "  \"filename\": \"" + filename + "\",\n" +
+                            "  \"labels\": [\n" +
+                            "    ");
+                    for (UriLabeler.Label label : labels) {
+                        textView.append(label.toString() + ",\n");
+                    }
+
+                    textView.append("  ]\n" +
+                            "}\n\n\n");
+                }
+
+                @Override
+                public void onFailure(Uri uri) {
+                    Log.e("URILABELER", uri.toString());
+                }
+            });
+
+            uriLabeler.processUri(getApplicationContext());
+        }
+    }
 }
---
+++ app/src/main/java/com/example/mlkittest/UriLabeler.java
@@ -0,0 +1,96 @@
+package com.example.mlkittest;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UriLabeler {
+    public interface UriLabelerListener {
+        void onSuccess(Uri uri, List<Label> labels);
+        void onFailure(Uri uri);
+    }
+
+    class Label {
+        String text;
+        String entityId;
+        Float confidence;
+
+        public Label(String text, String entityId, Float confidence) {
+            this.text = text;
+            this.entityId = entityId;
+            this.confidence = confidence;
+        }
+
+        @Override
+        public String toString() {
+            return "{ \"text\": \"" + text + "\", \"entityID\": \"" + entityId + "\", \"confidence\": " + confidence + " }";
+        }
+    }
+
+    private FirebaseVisionImage firebaseImage;
+    private FirebaseVisionImageLabeler labeler;
+
+    Uri uri;
+    List<Label> labels;
+    private UriLabelerListener listener;
+
+    public UriLabeler (Uri uri, UriLabelerListener listener) {
+        this.uri = uri;
+        this.labels = new ArrayList<Label>();
+
+        this.listener = listener;
+        labeler = FirebaseVision.getInstance().getOnDeviceImageLabeler();
+    }
+
+    public void processUri(Context context) {
+        try {
+            InputStream in = context.getContentResolver()
+                                    .openInputStream(uri);
+            Bitmap bitmap = BitmapFactory.decodeStream(in);
+            in.close();
+
+            firebaseImage = FirebaseVisionImage.fromBitmap(bitmap);
+        } catch (IOException e){
+            Log.e("MLKIT_INPUT_IMAGE", e.getMessage());
+        }
+
+        labels.clear();
+        labeler.processImage(firebaseImage)
+            .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
+                @Override
+                public void onSuccess(List<FirebaseVisionImageLabel> imagelabels) {
+                    for (FirebaseVisionImageLabel label : imagelabels) {
+                        String text = label.getText();
+                        String entityId = label.getEntityId();
+                        Float confidence = label.getConfidence();
+                        labels.add(new Label(text, entityId, confidence));
+                    }
+
+                    listener.onSuccess(uri, labels);
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e("FIREBASE_IMAGE_LABEL", e.getMessage());
+                    listener.onFailure(uri);
+                }
+            });
+    }
+}
