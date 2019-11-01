8329bf720435cba89122b4e79888fc14a8baffc6
Junyoung Park <carpe0308@naver.com>
2019-11-01 20:40:53 +0900

    Labeler 쓰레드 생성 액티비티 구현

--- app/src/main/java/com/example/mlkittest/MainActivity.java
+++ app/src/main/java/com/example/mlkittest/MainActivity.java
@@ -11,6 +11,7 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.OpenableColumns;
@@ -19,43 +20,40 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.mlkittest.Model.Label;
 import com.example.mlkittest.Model.TimeGroupAlgorithm;
 import com.example.mlkittest.Model.UnitImageFile;
 import com.example.mlkittest.Model.UnitImageFileGroup;
+import com.example.mlkittest.Util.ImageFileLabeler;
+import com.google.android.gms.vision.label.ImageLabeler;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     public static int PICK_IMAGE_MULTIPLE = 1;
-    ImageView imageView;
-    Button button;
-    TextView textView;
+    TextView textView1;
+    TextView textView2;
     Bitmap bitmap;
 
-    List<Uri> imageUris;
+    private int finishedTaskCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = findViewById(R.id.imageView);
-        button = findViewById(R.id.button);
-        textView = findViewById(R.id.textView);
+        textView1 = findViewById(R.id.textView1);
+        textView2 = findViewById(R.id.textView2);
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cat_dog);
 
-        button.setOnClickListener((View view) -> {
-            Intent intent = new Intent();
-            intent.setType("image/*");
-            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-            intent.setAction(Intent.ACTION_GET_CONTENT);
-            startActivityForResult(Intent.createChooser(intent,"Select Picture"), PICK_IMAGE_MULTIPLE);
-        });
-
+        //  파일 쓰기 권한 확인
         int permissionCheck = ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE);
 
         if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
@@ -68,26 +66,41 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+        //  이미지 -> 단위 이미지
         List<String> filePaths = getListOfFile();
-
         List<UnitImageFile> selectedImages = new ArrayList<>();
         for (String path: filePaths) {
             selectedImages.add(new UnitImageFile(path));
         }
 
+
+        //  시간 단위 클러스터링
         TimeGroupAlgorithm algorithm = new TimeGroupAlgorithm();
         List<UnitImageFileGroup> processedGroups = algorithm.processImages(selectedImages);
         for (int groupIdx = 0; groupIdx < processedGroups.size(); groupIdx++) {
-            textView.append("idx: " + groupIdx + "\n");
+            textView1.append("idx: " + groupIdx + "\n");
 
             List<UnitImageFile> images = processedGroups.get(groupIdx).getImages();
-            textView.append("length: " + images.size() + "\n");
+            textView1.append("length: " + images.size() + "\n");
 
             for (int imageIdx = 0; imageIdx < images.size(); imageIdx++) {
                 UnitImageFile image = images.get(imageIdx);
-                textView.append("filename: " + image.getFilename() + "\n");
+                textView1.append("filename: " + image.getFilename() + "\n");
             }
-            textView.append("\n\n\n");
+            textView1.append("\n\n\n");
+        }
+
+
+        //  시간 단위 그룹 레이블링
+        for (int groupIdx = 0; groupIdx < processedGroups.size(); groupIdx++) {
+            List<UnitImageFile> images = processedGroups.get(groupIdx).getImages();
+
+            new AsyncLabelingTask(() -> {
+                finishedTaskCount++;
+                if (finishedTaskCount % 20 == 0) {
+                    Toast.makeText(this, "그룹 " + finishedTaskCount + " 완료. ", Toast.LENGTH_SHORT).show();
+                }
+            }).execute(images);
         }
     }
 
@@ -117,51 +130,81 @@ public class MainActivity extends AppCompatActivity {
         return list;
     }
 
-    protected void processImagesWithMlkit() {
-        textView.clearComposingText();
-        for (Uri imageUri: imageUris) {
-            UriLabeler uriLabeler = new UriLabeler(imageUri, new UriLabeler.UriLabelerListener() {
-                @Override
-                public void onSuccess(Uri uri, List<UriLabeler.Label> labels) {
-                    String filename = null;
-                    if (uri.getScheme().equals("content")) {
-                        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-                        try {
-                            if (cursor != null && cursor.moveToFirst()) {
-                                filename = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
-                            }
-                        } finally {
-                            cursor.close();
-                        }
-                    }
+    public interface OnTaskCompleted {
+        void onTaskCompleted();
+    }
 
-                    if (filename == null) {
-                        filename = uri.getPath();
-                        int cut = filename.lastIndexOf('/');
-                        if (cut != -1) {
-                            filename = filename.substring(cut + 1);
-                        }
-                    }
 
-                    textView.append("{\n" +
-                            "  \"filename\": \"" + filename + "\",\n" +
-                            "  \"labels\": [\n" +
-                            "    ");
-                    for (UriLabeler.Label label : labels) {
-                        textView.append(label.toString() + ",\n");
-                    }
+    /**
+     * Thread for Async MLKit Image Labeler
+     */
+    private class AsyncLabelingTask extends AsyncTask<List<UnitImageFile>, Integer, List<List<Label>>> {
+        List<List<Label>> result = new ArrayList<>();
+        List<String> filenames = new ArrayList<>();
 
-                    textView.append("  ]\n" +
-                            "}\n\n\n");
+        // 쓰레드 종료 될 때 Callback listener
+        private OnTaskCompleted listener;
+
+        public AsyncLabelingTask(OnTaskCompleted listener){
+            this.listener = listener;
+        }
+
+        @Override
+        protected List<List<Label>> doInBackground(List<UnitImageFile>... files) {
+            List<List<Label>> fileLabels = processImagesWithMlkit(files[0]);
+            return fileLabels;
+        }
+
+        @Override
+        protected void onPostExecute(List<List<Label>> labels) {
+            super.onPostExecute(labels);
+
+            for (int lIdx = 0; lIdx < labels.size(); lIdx++) {
+                textView2.append("{\n" +
+                        "  \"filename\": \"" + filenames.get(lIdx) + "\"," +
+                        "  \"labels\": [\n" +
+                        "    ");
+                for (Label l: labels.get(lIdx)) {
+                    textView2.append(l.toString() + ",\n");
                 }
+                textView2.append("  ]\n" +
+                        "}\n\n\n");
+            }
+            textView2.append("/*------------------------------------------*/");
+            listener.onTaskCompleted();
+        }
 
-                @Override
-                public void onFailure(Uri uri) {
-                    Log.e("URILABELER", uri.toString());
-                }
-            });
+        private List<List<Label>> processImagesWithMlkit(List<UnitImageFile> imageFiles) {
+            final CountDownLatch latch = new CountDownLatch(imageFiles.size());
+            for (UnitImageFile imageFile: imageFiles) {
+                File file = imageFile.getFile();
 
-            uriLabeler.processUri(getApplicationContext());
+                ImageFileLabeler imageFileLabeler = new ImageFileLabeler(file, new ImageFileLabeler.ImageFileLabelerListener() {
+                    @Override
+                    public void onSuccess(File file, List<Label> labels) {
+                        result.add(labels);
+                        filenames.add(file.getName());
+
+                        latch.countDown();
+                    }
+
+                    @Override
+                    public void onFailure(File file) {
+                        Log.e("ImageFileLabeler", file.getName());
+                        latch.countDown();
+                    }
+                });
+
+                imageFileLabeler.process();
+            }
+
+            try {
+                latch.await(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                Log.e("MLKIT_ASYNC_TASK", e.toString());
+            }
+
+            return result;
         }
     }
 }
